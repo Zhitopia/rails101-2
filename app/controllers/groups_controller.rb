0deef77123221fe62b76b38f7308e4354fd802e9
Zhang mickey <Zhitopia@Gmail.com>
2017-06-23 13:52:20 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,10 @@
 class GroupsController < ApplicationController
-
-   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
-   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @groups = Group.all
-  end
+      @groups = Group.all
+    end
 
   def show
     @group = Group.find(params[:id])
@@ -28,7 +27,7 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-    
+
   end
 
   def update
