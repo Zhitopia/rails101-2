d7d1228ece3feff7786bdb23cdd28cd00ef8cd5b
Zhang mickey <Zhitopia@Gmail.com>
2017-06-23 14:31:51 +0800

    implement #posts edit & update & destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -20,6 +25,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+          redirect_to account_posts_path, notice: "Updated Success!"
+      else
+          render :edit
+      end
+  end
 
   private
 
